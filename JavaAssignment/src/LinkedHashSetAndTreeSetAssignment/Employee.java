717b1f3797d68631ed055814c6613a5845f5a21c
Abhiattarge <attargeabhi@gmail.com>
2022-05-21 17:18:24 +0530

    Added LinkedHashSetAssignment Added TreeSetAssignment

---
+++ JavaAssignment/src/LinkedHashSetAndTreeSetAssignment/Employee.java
@@ -0,0 +1,15 @@
+package LinkedHashSetAndTreeSetAssignment;
+
+public class Employee {
+	int empId;
+	String name;
+	float sal;
+	Employee(int empId, String name, float sal){
+		this.empId = empId;
+		this.name = name;
+		this.sal = sal;
+	}
+	public String toString() {
+		return "[ empId : "+ empId + ", name : "+ name +", sal : "+ sal+" ]";
+	}
+}
